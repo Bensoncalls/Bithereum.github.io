e6fb0faf41bd583d001163931092d84f0abcf79f
Daniel <developer@daniel-espanadero.com>
2023-08-20 23:55:01 +0200

    Imported header module into the app component

--- src/app/modules/header/header.module.ts
+++ src/app/modules/header/header.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-import { NavComponent } from './nav/nav.component';
-
 
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    NavComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent],
 })
-export class HeaderModule { }
+export class HeaderModule {}
