771ffb3fe1b1410076a451a5d0392f2622ff6a0c
Daniel <developer@daniel-espanadero.com>
2023-08-20 23:18:08 +0200

    Created modules and components

---
+++ src/app/modules/instructions/instructions.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InstructionsComponent } from './instructions/instructions.component';
+
+
+
+@NgModule({
+  declarations: [
+    InstructionsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class InstructionsModule { }
---
+++ src/app/modules/trends/trends.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TrendsComponent } from './trends/trends.component';
+
+
+
+@NgModule({
+  declarations: [
+    TrendsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TrendsModule { }
