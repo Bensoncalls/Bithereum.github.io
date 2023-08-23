6c5f0ac284fb659d0b90e2243aa0ecf54f493789
Daniel <developer@daniel-espanadero.com>
2023-08-23 20:13:15 +0200

    How it work area started

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from './home/home.component';
 import { NavModule } from '../nav/nav.module';
 import { HeaderModule } from '../header/header.module';
 import { HomeRoutingModule } from './home-routing.module';
+import { InstructionsModule } from '../instructions/instructions.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, NavModule, HeaderModule, HomeRoutingModule],
+  imports: [CommonModule, NavModule, HeaderModule, HomeRoutingModule, InstructionsModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
--- src/app/modules/instructions/instructions.module.ts
+++ src/app/modules/instructions/instructions.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InstructionsComponent } from './instructions/instructions.component';
-
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @NgModule({
-  declarations: [
-    InstructionsComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [InstructionsComponent],
+  imports: [CommonModule, TranslateModule],
+  exports: [InstructionsComponent]
 })
-export class InstructionsModule { }
+export class InstructionsModule {}
