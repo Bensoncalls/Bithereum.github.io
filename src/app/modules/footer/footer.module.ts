ce0e36d68ee58f1152cf5f2ebda9fb524c1ef9d1
Daniel <developer@daniel-espanadero.com>
2023-08-23 19:48:02 +0200

    Footer created and added new images in assets folder

--- src/app/modules/footer/footer.module.ts
+++ src/app/modules/footer/footer.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
-
-
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule
   ],
   exports:[FooterComponent]
 })
