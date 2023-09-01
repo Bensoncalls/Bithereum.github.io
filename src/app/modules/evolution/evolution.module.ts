69859d3b0d6a6d37d26b724452c00288e5fa8076
Daniel <developer@daniel-espanadero.com>
2023-09-01 23:26:20 +0200

    Created modume and service 'evolution'

---
+++ src/app/modules/evolution/evolution.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EvolutionComponent } from './evolution/evolution.component';
+
+
+
+@NgModule({
+  declarations: [
+    EvolutionComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class EvolutionModule { }
